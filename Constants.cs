e5bfbf285e849ec2a386167b73ef930a765edeb0
Ashish-Kr <ashish25@outlook.com>
2015-10-28 17:53:15 -0400

    Create Constants.cs

---
+++ Constants.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace SenTest
+{
+    public static class Constants
+    {
+        /* Configure the MobileServiceClient to communicate with your mobile service by
+         uncommenting the following code and replacing AppUrl & AppKey with values from  
+        your mobile service, which are obtained from the Windows Azure Management Portal.
+	*/
+        // Azure app specific URL and key
+	public const string ApplicationURL = @"https://sensortest.azure-mobile.net/";
+	public const string ApplicationKey = @"HCNvTLmxdyMUJRbczuqndkWBRIgWxN63";
+    }
+}
+
